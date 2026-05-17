2f41fd6820f987d90ef7fb269446da302d248502
brbucknell <brbucknell18@gmail.com>
2026-05-17 12:14:17 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -216,7 +216,7 @@ export default function Home() {
 
                       <div className="flex w-full max-w-[54px] flex-col overflow-hidden rounded-sm shadow-md">
                         <div
-                          className="bg-green-500"
+                          className="bg-green-1000"
                           style={{
                             height: `${
                               (bar.segments[0] / 1000) * chartHeight
@@ -224,7 +224,7 @@ export default function Home() {
                           }}
                         />
                         <div
-                          className="bg-blue-700"
+                          className="bg-blue-1000"
                           style={{
                             height: `${
                               (bar.segments[1] / 1000) * chartHeight
@@ -232,7 +232,7 @@ export default function Home() {
                           }}
                         />
                         <div
-                          className="bg-red-500"
+                          className="bg-red-1000"
                           style={{
                             height: `${
                               (bar.segments[2] / 1000) * chartHeight
